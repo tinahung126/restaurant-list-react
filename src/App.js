6e321d62c7dc510995e0e501d7d92752c9aecda1
Tina <tina126hung@gmail.com>
2021-09-04 15:12:56 +0800

    Feat: Add list card layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='row'>
+        <div className=' col-11  mx-auto'>
+          <div className=' row'>
+            <div className='col-xl-3 col-lg-4 col-sm-6 col-12 '>
+              <div className='card mb-3 '>
+                <img
+                  className='card-img-top'
+                  src='https://picsum.photos/500/400?random=1'
+                  alt=''
+                />
+                <div className='card-body'>
+                  <h5 className='card-title'>石二鍋</h5>
+                  <h6 className='card-subtitle my-2'>經典料理都在這裡</h6>
+                </div>
+                <div className='card-body d-flex justify-content-between align-items-center'>
+                  <span>1471人在追蹤</span>
+                  <a
+                    href='#' className='btn dark  px-6'
+                    style={{ backgroundColor: '#844224', color: 'white' }}
+                  >追蹤
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className='col-xl-3 col-lg-4 col-sm-6 col-12'>
+              <div className='card mb-3 '>
+                <img
+                  className='card-img-top'
+                  src='https://picsum.photos/500/400?random=1'
+                  alt=''
+                />
+                <div className='card-body'>
+                  <h5 className='card-title'>石二鍋</h5>
+                  <h6 className='card-subtitle my-2'>經典料理都在這裡</h6>
+                </div>
+                <div className='card-body d-flex justify-content-between align-items-center'>
+                  <span>1471人在追蹤</span>
+                  <a
+                    href='#' className='btn dark  px-6'
+                    style={{ backgroundColor: '#844224', color: 'white' }}
+                  >追蹤
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className='col-xl-3 col-lg-4 col-sm-6 col-12'>
+              <div className='card mb-3'>
+                <img
+                  className='card-img-top'
+                  src='https://picsum.photos/500/400?random=1'
+                  alt=''
+                />
+                <div className='card-body'>
+                  <h5 className='card-title'>石二鍋</h5>
+                  <h6 className='card-subtitle my-2'>經典料理都在這裡</h6>
+                </div>
+                <div className='card-body d-flex justify-content-between align-items-center'>
+                  <span>1471人在追蹤</span>
+                  <a
+                    href='#' className='btn dark  px-6'
+                    style={{ backgroundColor: '#844224', color: 'white' }}
+                  >追蹤
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className='col-xl-3 col-lg-4 col-sm-6 col-12'>
+              <div className='card mb-3 '>
+                <img
+                  className='card-img-top'
+                  src='https://picsum.photos/500/400?random=1'
+                  alt=''
+                />
+                <div className='card-body'>
+                  <h5 className='card-title'>石二鍋</h5>
+                  <h6 className='card-subtitle my-2'>經典料理都在這裡</h6>
+                </div>
+                <div className='card-body d-flex justify-content-between align-items-center'>
+                  <span>1471人在追蹤</span>
+                  <a
+                    href='#' className='btn dark  px-6'
+                    style={{ backgroundColor: '#844224', color: 'white' }}
+                  >追蹤
+                  </a>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
-  );
+
+  )
 }
 
-export default App;
+export default App
